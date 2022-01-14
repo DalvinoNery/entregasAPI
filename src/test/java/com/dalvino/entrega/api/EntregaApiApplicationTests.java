f255eea5f66c33e505fa3a9cb521d3ca1f1d3f1e
DalvinoNery <rodrigo.s.nery@gmail.com>
2022-01-14 20:46:10 -0300

    Aula 01

---
+++ src/test/java/com/dalvino/entrega/api/EntregaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalvino.entrega.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
